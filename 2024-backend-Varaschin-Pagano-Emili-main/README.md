d343be93e4dff97fba75d9517ca197d5e02b29bc
Evaraschin <116093531+Evaraschin@users.noreply.github.com>
2024-11-20 00:50:23 -0300

    Delete 2024-backend-Varaschin-Pagano-Emili-main directory

--- 2024-backend-Varaschin-Pagano-Emili-main/README.md
+++
@@ -1,2 +0,0 @@
-# 2024-backend-Varaschin-Pagano-Emili
-2024-backend-Evaraschin created by GitHub Classroom
--- 2024-backend-Varaschin-Pagano-Emili-main/src/routes/orders.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ordersController = require('../controllers/orders.controller');
-const { verifyToken } = require('../middleware/auth.middleware');
-
-router.post('/', verifyToken, ordersController.createOrder);
-router.get('/user/:userId', verifyToken, ordersController.getOrdersByUser);
-
-module.exports = router;
