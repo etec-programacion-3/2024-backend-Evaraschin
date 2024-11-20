9bdee26ac0bf1d7dfa2e22f501681809b5505ccf
Evaraschin <116093531+Evaraschin@users.noreply.github.com>
2024-11-20 10:52:30 -0300

    Delete src directory

--- src/routes/games.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const gamesController = require('../controllers/games.controller');
-const { verifyToken } = require('../middleware/auth.middleware');
-
-router.get('/', gamesController.getAllGames);
-router.get('/:id', gamesController.getGameById);
-
-module.exports = router;
